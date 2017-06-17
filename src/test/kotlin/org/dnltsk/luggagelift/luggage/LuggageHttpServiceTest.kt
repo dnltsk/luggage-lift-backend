17323f96a5d9498481e62b082dd5ab81478055ed
danielt <daniel.teske@gmail.com>
2017-06-17 23:02:36 +0200

    setup to reset luggageState for all tests

--- src/test/kotlin/org/dnltsk/luggagelift/luggage/LuggageHttpServiceTest.kt
+++ src/test/kotlin/org/dnltsk/luggagelift/luggage/LuggageHttpServiceTest.kt
@@ -3,6 +3,7 @@ package org.dnltsk.luggagelift.luggage
 import org.assertj.core.api.Assertions.assertThat
 import org.dnltsk.luggagelift.luggage.LuggageStatus.*
 import org.dnltsk.luggagelift.order.confirm.OrderConfirmResponse
+import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.springframework.beans.factory.annotation.Autowired
@@ -17,6 +18,11 @@ class LuggageHttpServiceTest {
 
     @Autowired
     private val restTemplate: TestRestTemplate? = null
+
+    @Before
+    fun setUp() {
+        val orderConfirmResonse = this.restTemplate!!.getForObject("/order/confirm", OrderConfirmResponse::class.java)
+    }
 
     @Test
     fun `initial status is DRIVER_IN_TRANSIT`() {
@@ -33,13 +39,16 @@ class LuggageHttpServiceTest {
         assertThat(body2).isEqualTo(LuggageStatusResponse(luggageStatus = ON_PLAN))
     }
 
-    @Test
+    @Test //@Ignore
     fun `new order confirmation rests the luggage status`() {
+        //Do some change
         val body1 = this.restTemplate!!.postForObject("/luggage?luggageStatus=${BEHIND_SECURITY_CONTROL.name}", null, LuggageStatusResponse::class.java)
         assertThat(body1).isEqualTo(LuggageStatusResponse(luggageStatus = BEHIND_SECURITY_CONTROL))
 
+        //confirm a new order
         val orderConfirmResonse = this.restTemplate!!.getForObject("/order/confirm", OrderConfirmResponse::class.java)
 
+        //luggage status must be reset
         val body2 = this.restTemplate!!.getForObject("/luggage", LuggageStatusResponse::class.java)
         assertThat(body2).isEqualTo(LuggageStatusResponse(luggageStatus = DRIVER_IN_TRANSIT))
     }
